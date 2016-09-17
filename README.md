7544258f0911a04d0e21a5b129606bda303ccc37
Kalle Hjerppe <kalle.hjerppe@solita.fi>
2016-09-17 17:21:36 +0300

    initial version

---
+++ README.md
@@ -0,0 +1,33 @@
+# Pipeline Performace Tester
+This is a work in progress tool to run phantomas tests in CI with controllable targets and criteria for passing.
+
+##Dependencies
+- node (developed on 4.2.3)
+- phantomas module
+
+## Installing
+```
+npm install
+```
+
+## Configuration
+Configuring the tests is done using the following JSON files:
+```
+target_urls.json
+```
+Should contain an array holding the (full) URLs to run the tests against.
+
+```
+limits_default.json
+```
+Holds all testable variables with sensible *(TODO)* default values. The results for phantomas tests must be equal or less than the tresholds specified to pass.
+
+```
+limits_override.json
+```
+Writing limit-keys to this file overrides any specified in `limits_default`. This is intended for project-specific settings.
+
+## Running
+```
+node perftest
+```
---
+++ perftest.js
@@ -0,0 +1,67 @@
+var phantomas = require('phantomas');
+var fs = require('fs');
+
+var performTest = function(url, limits){
+    return new Promise((resolve, reject) => {
+        phantomas(url, {"analyze-css": true}, function(err, json, results) {
+            if(err) reject(err);
+            var failures = [];
+            var result = json.metrics;
+            for(var prop in limits){
+                if(result[prop] > limits[prop]){
+                    failures.push({"url": url, "testCase": prop, "result": result[prop], "limit": limits[prop]});
+                }
+            }
+            resolve(failures);
+        });
+    });
+}
+
+var readConfiguration = function(testFunction){
+    fs.readFile('limits_default.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        var config = JSON.parse(data);
+        fs.readFile('limits_override.json', 'utf8', function(err, data) {
+            if (err) throw err;
+            var override = JSON.parse(data);
+            for(var prop in override){
+                config[prop] = override[prop];
+            }
+            readTargetUrls(function(urls){
+                testFunction(config, urls);
+            });
+        });
+    });
+}
+
+var readTargetUrls = function(callback){
+    fs.readFile('target_urls.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        callback(JSON.parse(data));
+    });
+}
+
+var main = function(limits, urls){
+    console.log("Running performance test suite");
+    var promises = urls.map(url => {return performTest(url, limits)});
+    Promise.all(promises)
+        .then(failuresByUrl => {
+            var failures = failuresByUrl.reduce(
+                (sum, part) => {
+                    return sum.concat(part);
+                }, []);
+            failures.forEach(failure => {
+                console.log(["Failed test", failure]);
+            });
+            var passed = failures.length === 0;
+            console.log(passed 
+                ? "Tests successful!" 
+                : "Tests FAILED: " + failures.length);
+            process.exit(passed ? 0 : 1)
+        })
+        .catch(error => { 
+            console.log(error);
+            process.exit(1);
+        });
+}
+readConfiguration(main);                                                                                                                             
